6e4d46c8a48ed033bb712dacc3c28f7e51561808
noheul1030 <papooa5140@gmail.com>
2023-09-26 16:44:08 +0900

    Update ControllerTest.java

--- workspace2/ResortProject/src/main/java/com/resort/springboot/web/ControllerTest.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/web/ControllerTest.java
@@ -3,54 +3,67 @@ package com.resort.springboot.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @Controller
 public class ControllerTest {
 
-	
+	// =============
+	// 헤더, 푸터
+	@GetMapping("/header")
+	public String header() {
+		return "fragments/header";
+	}
+
+	@GetMapping("/footer")
+	public String footer() {
+		return "fragments/footer";
+	}
+	// =============
+
 	@GetMapping("/main")
 	public String main() {
 		return "main";
 	}
-	@GetMapping("/about")
-	public String about() {
-		return "about";
+	
+	@GetMapping("/location")
+	public String location() {
+		return "location";
 	}
-	@GetMapping("/contact")
-	public String contact() {
-		return "contact";
+
+	@GetMapping("/noticeBoard")
+	public String noticeBoard() {
+		return "noticeBoard";
 	}
+
 	@GetMapping("/index")
 	public String index() {
 		return "index";
 	}
+
 	@GetMapping("/index_1")
 	public String index1() {
 		return "index_1";
 	}
+
 	@GetMapping("/properties")
 	public String properties() {
 		return "properties";
 	}
+
 	@GetMapping("/propertySingle")
 	public String propertySingle() {
 		return "property-single";
 	}
+
 	@GetMapping("/services")
 	public String services() {
 		return "services";
 	}
-
 	
-	@GetMapping("/resortMain")
-	public String resortMain() {
-		return "resortMain";
-	}
 	@GetMapping("/signUp")
 	public String signUp() {
 		return "signUp";
 	}
+
 	@GetMapping("/login")
 	public String login() {
 		return "login";
