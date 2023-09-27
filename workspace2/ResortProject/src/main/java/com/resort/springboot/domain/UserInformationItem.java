b27c8937f26f9a34085c9b00c97514bef2424244
noheul1030 <papooa5140@gmail.com>
2023-09-27 21:00:38 +0900

    FEAT: UserInformationItem DB 수정
    1. userNumber - pk
    2. 회원 id - unique
    3. 회원 비밀번호
    4. 이메일 - unique
    5. 회원 이름
    6. 성별
    7. 전화번호
    8. 권한

--- workspace2/ResortProject/src/main/java/com/resort/springboot/domain/UserInformationItem.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/domain/UserInformationItem.java
@@ -5,24 +5,41 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
+@Data
 public class UserInformationItem {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	private Long userNumber;
 	
-	@Column(length = 30, nullable = false)
+	// 회원 ID
+	@Column(length = 50, nullable = false, unique = true)
 	private String id;
 	
+	// 회원 비밀번호
 	@Column(nullable = false)
 	private String passward;
 	
+	// 이메일
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	// 회원 이름
+	@Column(nullable = false)
+	private String name;
+	
+	// 성별
+	@Column(nullable = false)
+	private String sex;
+	
+	// 전화번호
+	@Column(nullable = false)
+	private String phoneNumber;
+	
+	// 권한
 	@Column(nullable = false)
 	private String authority;
 	
