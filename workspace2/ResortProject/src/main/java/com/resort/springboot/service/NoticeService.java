79215a201a7b4d2d6fd52e53b8627cabd0000a5c
noheul1030 <papooa5140@gmail.com>
2023-10-06 17:46:23 +0900

    Update NoticeService.java
    - 공지사항 인터페이스 구현 준비

--- workspace2/ResortProject/src/main/java/com/resort/springboot/service/NoticeService.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/service/NoticeService.java
@@ -1,5 +1,26 @@
 package com.resort.springboot.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.resort.springboot.domain.Notice;
+
 public interface NoticeService {
+
+//	LocalDate date();
+//
+//	void insertTest();
+//
+//	void newInsert(Notice notice);
+//
+//	List<Notice> findAll();
+//
+//	void deleteId(Long noticeId);
+//
+//	Notice oneSelectView(Long noticeId);
+//
+//	void visit(Long noticeId);
+//
+//	void update(Notice notice);
 
 }
