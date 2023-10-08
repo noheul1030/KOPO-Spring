e08faebaa10502ea0adb4641395264e0d97eec5c
noheul1030 <papooa5140@gmail.com>
2023-10-08 23:13:53 +0900

    Feat: noticeBoard 기능구현
    - 전체 리스트 조회
    - 새로운 글 작성
    - 하나의 글 조회

--- workspace2/ResortProject/src/main/java/com/resort/springboot/service/NoticeService.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/service/NoticeService.java
@@ -1,26 +1,121 @@
 package com.resort.springboot.service;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.resort.springboot.domain.Notice;
+import com.resort.springboot.exception.DataNotFoundException;
+import com.resort.springboot.repo.NoticeRepository;
 
-public interface NoticeService {
+import lombok.RequiredArgsConstructor;
 
-//	LocalDate date();
-//
-//	void insertTest();
-//
-//	void newInsert(Notice notice);
-//
-//	List<Notice> findAll();
-//
-//	void deleteId(Long noticeId);
-//
-//	Notice oneSelectView(Long noticeId);
-//
-//	void visit(Long noticeId);
-//
-//	void update(Notice notice);
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class NoticeService {
 
+	private NoticeRepository noticeRepository;
+
+	@Autowired
+	public NoticeService(NoticeRepository noticeRepository) {
+		this.noticeRepository = noticeRepository;
+	}
+
+//
+//	// 날짜
+//	@Override
+//	public LocalDate date() {
+//		LocalDate now = LocalDate.now();
+//		return now;
+//	}
+//
+//	// 게시글 임의등록 Test
+//	@Override
+//	public void insertTest() {
+//		for (int i = 1; i <= 20; i++) {
+//			Notice notice = new Notice();
+//
+//			notice.setDate(String.valueOf(date()));
+//			notice.setContent("Test 게시글 입니다.");
+//			notice.setTitle("Test Title 입니다.");
+//			notice.setViewcnt(0);
+//
+//			noticeRepository.save(notice);
+//		}
+//	}
+//
+	// New 게시글 등록
+	public void newInsert(String title, String content, String id) {
+		
+		Notice notice = new Notice();
+
+		notice.setId(id);
+		notice.setTitle(title);
+		notice.setContent(content);
+		
+		// 현재 날짜를 LocalDateTime 객체로 가져오기
+		LocalDateTime currentDate = LocalDateTime.now();
+
+		// LocalDateTime 객체를 원하는 형식의 문자열로 변환하기
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // 원하는 형식으로 포맷 지정
+		String date = currentDate.format(formatter);
+
+		notice.setDate(date);
+		notice.setViewcnt(0);
+		this.noticeRepository.save(notice);
+
+		noticeRepository.save(notice);
+	}
+
+//	
+//	@Override // 컬럼 값 수정 update
+//	public void update(Notice notice) {
+//		Notice notice2 = noticeRepository.findByNoticeId(notice.getNoticeId()).get();
+//		// 가져온 글에 수정한 내용을 세팅한다.
+//		notice2.setTitle(notice.getTitle());
+//		notice2.setContent(notice.getContent());
+//
+//        // DB에 저장
+//		noticeRepository.save(notice2);
+//	}
+//
+	// 전체 게시물 조회
+	public List<Notice> getList() {
+//		PageRequest pageable = PageRequest.of(0, 10); // 페이지 번호 0부터 시작
+//		Page<BoardItem> page = boardRepository.findAll(pageable);
+//		return page.getContent();
+		return this.noticeRepository.findAll();
+	}
+
+//
+//	// id값으로 삭제
+//	@Override
+//	public void deleteId(Long id) {
+//		noticeRepository.deleteByNoticeId(id);
+//	}
+//
+	// id값으로 한건 조회
+	public Notice oneSelectView(Long noticeId) {
+		Optional<Notice> notice = this.noticeRepository.findByNoticeId(noticeId);
+		if (notice.isPresent()) {
+			return notice.get();
+		} else {
+			throw new DataNotFoundException("notice not found");
+		}
+	}
+//
+//	@Override // id값으로 한건 조회 시 조회수 카운트
+//	public void visit(Long noticeId) {
+//		Notice notice = noticeRepository.findByNoticeId(noticeId).orElse(null);
+//		if (notice != null) {
+//			notice.setViewcnt(notice.getViewcnt() + 1);
+//			noticeRepository.save(notice);
+//		}
+//	}
 }
--- workspace2/ResortProject/src/main/java/com/resort/springboot/web/NoticeController.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/web/NoticeController.java
@@ -4,33 +4,54 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.resort.springboot.domain.Notice;
-import com.resort.springboot.service.NoticeServiceImpl;
+import com.resort.springboot.dto.NoticeDto;
+import com.resort.springboot.service.NoticeService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-@RequestMapping("/notice")
 @RequiredArgsConstructor
 @Controller
 public class NoticeController {
 
-	private final NoticeServiceImpl noticeServiceImpl;
+	private final NoticeService noticeService;
 
-	@GetMapping("/list")
+	@GetMapping("/noticeBoard_list")
 	public String list(Model model) {
 
-		List<Notice> noticeList = this.noticeServiceImpl.getList();
+		List<Notice> noticeList = this.noticeService.getList();
 		model.addAttribute("noticeList", noticeList);
-		return "/notice/list";
+		return "/noticeBoard_list";
+	}
+
+	@GetMapping("/noticeBoard_create")
+	public String notice(Model model) {
+		model.addAttribute("noticeForm", new NoticeDto());
+		
+		return "noticeBoard_create";
+	}
+
+	@PostMapping("/noticeBoard_create")
+	public String noticeCreate(@Valid @ModelAttribute("noticeForm") NoticeDto noticeDto, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			return "noticeBoard_create";
+		}
+		
+		this.noticeService.newInsert(noticeDto.getTitle(), noticeDto.getContent(), noticeDto.getId());
+		return "redirect:/noticeBoard_list"; // 질문 저장후 질문목록으로 이동
 	}
 
 	@GetMapping(value = "/detail/{noticeId}")
 	public String detail(Model model, @PathVariable("noticeId") Long id) {
-		Notice notice = this.noticeServiceImpl.oneSelectView(id);
+		Notice notice = this.noticeService.oneSelectView(id);
 		model.addAttribute("notice", notice);
 		return "/notice/detail";
 	}
