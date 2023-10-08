945b68c2953b99d352d6f66cd689570663eb3657
noheul1030 <papooa5140@gmail.com>
2023-10-08 23:08:14 +0900

    Feat: NoticeDto 구현

--- workspace2/ResortProject/src/main/java/com/resort/springboot/dto/NoticeDto.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/dto/NoticeDto.java
@@ -2,6 +2,45 @@ package com.resort.springboot.dto;
 
 import com.resort.springboot.domain.Notice;
 
-public class NoticeDto extends Notice{
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class NoticeDto {
+
+	private Long noticeId;
+	
+	private String id;
+
+	@Pattern(regexp = "^[가-힣a-zA-Z]$", message = "제목의 입력값이 유효하지 않습니다. 다시 확인해 주세요.")
+	@NotEmpty(message = "제목은 필수항목입니다.")
+	private String title;
+	
+	@NotEmpty(message="내용은 필수항목입니다.")
+	private String content;
+	
+	private String date;	
+	
+	private Integer viewcnt;
+
+	public Notice createNotice() {
+		Notice notice = Notice.builder()
+				.noticeId(noticeId)
+				.id(id)
+	    		.title(title)
+	    		.date(date)
+	            .content(content)
+	            .viewcnt(0)
+	            .build();
+	    return notice;
+	}
 }
