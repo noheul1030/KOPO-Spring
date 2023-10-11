d0ce08d31ec3e4f0617baa8b66249a455cb33f32
noheul1030 <papooa5140@gmail.com>
2023-10-11 21:19:35 +0900

    Feat: 댓글 기능 준비

--- workspace2/ResortProject/src/main/java/com/resort/springboot/repo/NoticeCommentRepository.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/repo/NoticeCommentRepository.java
@@ -1,11 +1,16 @@
 package com.resort.springboot.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.resort.springboot.domain.Notice;
 import com.resort.springboot.domain.NoticeComment;
 
 @Repository
 public interface NoticeCommentRepository extends JpaRepository<NoticeComment,Long>{
+	
+	List<NoticeComment> getCommentByRootIdOrderByCommentId(Notice notice);
 
 }
