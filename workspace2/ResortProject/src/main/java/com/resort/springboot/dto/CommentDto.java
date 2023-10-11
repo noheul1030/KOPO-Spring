b909180fa13331d783b055ed34352afbe203b91a
noheul1030 <papooa5140@gmail.com>
2023-10-11 21:19:06 +0900

    Update: DB Join DTO 수정

--- workspace2/ResortProject/src/main/java/com/resort/springboot/dto/CommentDto.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/dto/CommentDto.java
@@ -1,32 +1,65 @@
 package com.resort.springboot.dto;
 
+import java.time.LocalDateTime;
+
+import com.resort.springboot.domain.Notice;
 import com.resort.springboot.domain.NoticeComment;
+import com.resort.springboot.domain.SiteUser;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class CommentDto {
 
-//	private String noticeId;
+	// 댓글 서비스 요청
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	@Getter
+	@Setter
+	public static class Request {
 
-	private Long reid;
+		private Long commentId;
+		private LocalDateTime date;
+		private LocalDateTime commentModifiedDate;
+		private Notice rootId;
+		private SiteUser commentUser;
 
-	@NotEmpty(message = "내용은 필수항목입니다.")
-	private String recontent;
+		@NotEmpty(message = "내용은 필수항목입니다.")
+		private String comment;
 
-	private String redate;
+		// Dto -> Entity
+		public NoticeComment toEntity() {
+			NoticeComment comments = NoticeComment.builder().commentId(commentId).comment(comment).date(date)
+					.commentModifiedDate(commentModifiedDate).rootId(rootId).commentUser(commentUser).build();
 
-	
-	public NoticeComment createComment() {
-		NoticeComment noticeComment = NoticeComment.builder()
-//				.noticeId(noticeId)
-				.reid(reid)
-	    		.recontent(recontent)
-	    		.redate(redate)
-	            .build();
-	    return noticeComment;
+			return comments;
+		}
+	}
+
+	// 댓글 정보 반환
+	@Getter
+	public static class Response {
+
+		private Long commentId;
+		private String comment;
+		private LocalDateTime date;
+		private LocalDateTime commentModifiedDate;
+		private Notice rootId;
+		private SiteUser commentUser;
+
+		// Entity -> Dto
+		public Response(NoticeComment comment) {
+			this.commentId = comment.getCommentId();
+			this.comment = comment.getComment();
+			this.date = comment.getDate();
+			this.commentModifiedDate = comment.getCommentModifiedDate();
+			this.rootId = comment.getRootId();
+			this.commentUser = comment.getCommentUser();
+		}
 	}
 }
--- workspace2/ResortProject/src/main/java/com/resort/springboot/dto/NoticeDto.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/dto/NoticeDto.java
@@ -1,46 +1,86 @@
 package com.resort.springboot.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import com.resort.springboot.domain.Notice;
+import com.resort.springboot.domain.NoticeComment;
+import com.resort.springboot.domain.SiteUser;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Getter
-@Setter
 public class NoticeDto {
 
-	private Long noticeId;
-	
-	private String id;
+	@AllArgsConstructor
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@Builder
+	@Getter
+	@Setter
+	public static class Request {
+		private Long noticeId;
 
-	@Pattern(regexp = "^[가-힣a-zA-Z0-9?!,&]+( [가-힣a-zA-Z0-9?!,&]+)*$", message = "제목의 입력값이 유효하지 않습니다. 다시 확인해 주세요.")
-	@NotEmpty(message = "제목은 필수항목입니다.")
-	private String title;
-	
-	@NotEmpty(message="내용은 필수항목입니다.")
-	private String content;
-	
-	private String date;	
-	
-	private Integer viewcnt;
+		private SiteUser id;
 
-	public Notice createNotice() {
-		Notice notice = Notice.builder()
-				.noticeId(noticeId)
-				.id(id)
-	    		.title(title)
-	    		.date(date)
-	            .content(content)
-	            .viewcnt(0)
-	            .build();
-	    return notice;
+		@Pattern(regexp = "^[가-힣a-zA-Z0-9?!,&]+( [가-힣a-zA-Z0-9?!,&]+)*$", message = "제목의 입력값이 유효하지 않습니다. 다시 확인해 주세요.")
+		@NotEmpty(message = "제목은 필수항목입니다.")
+		private String title;
+
+		@NotEmpty(message = "내용은 필수항목입니다.")
+		private String content;
+
+		private LocalDateTime date;
+
+		private LocalDateTime postModifiedDate;
+
+		private Integer viewcnt;
+		
+		private List<NoticeComment> comments;
+
+		public Notice createNotice() {
+			Notice notice = Notice.builder()
+					.noticeId(noticeId)
+					.id(id)
+					.title(title)
+					.date(date)
+					.postModifiedDate(postModifiedDate)
+					.content(content)
+					.viewcnt(0)
+					.comments(comments)
+					.build();
+			return notice;
+		}
 	}
+	
+	// 게시글 정보 반환
+		@Getter
+	    public static class Response {
+			
+			private final Long noticeId;
+			private final SiteUser id;
+			private final String title;
+			private final String content;
+			private final LocalDateTime date;
+			private final LocalDateTime postModifiedDate;
+			private final int viewcnt;
+			private List<NoticeComment> comments;
+			
+			// Entity -> Dto
+	        public Response(Notice notice) {	   
+	            this.noticeId = notice.getNoticeId();
+	            this.title = notice.getTitle();
+	            this.date = notice.getDate();
+	            this.postModifiedDate = notice.getPostModifiedDate();
+	            this.content = notice.getContent();
+	            this.viewcnt = notice.getViewcnt();
+	            this.id = notice.getId();
+	            this.comments = notice.getComments();
+	        }
+		}
 }
