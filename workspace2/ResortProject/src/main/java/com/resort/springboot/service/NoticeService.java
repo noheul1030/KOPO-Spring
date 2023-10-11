646b132d26b54b94cd71afee9530cddb9c611aae
noheul1030 <papooa5140@gmail.com>
2023-10-11 21:20:13 +0900

    Update NoticeService.java

--- workspace2/ResortProject/src/main/java/com/resort/springboot/service/NoticeService.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/service/NoticeService.java
@@ -1,18 +1,21 @@
 package com.resort.springboot.service;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.resort.springboot.domain.Notice;
+import com.resort.springboot.domain.SiteUser;
+import com.resort.springboot.exception.DataNotFoundException;
 import com.resort.springboot.repo.NoticeRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -20,77 +23,94 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class NoticeService {
 
-	private NoticeRepository noticeRepository;
-
-	@Autowired
-	public NoticeService(NoticeRepository noticeRepository) {
-		this.noticeRepository = noticeRepository;
+	private final NoticeRepository noticeRepository;
+	
+	public Notice getNotice(long noticeId) {
+		Optional<Notice> notice = this.noticeRepository.findById(noticeId);
+		if (notice.isPresent()) {
+			return notice.get();
+		} else {
+			throw new DataNotFoundException("post not found");
+		}
 	}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// New 게시글 등록
-	public void newInsert(Notice notice) {
-		
+	/* CREATE */
+	// 1. New 게시글 등록
+	public void newInsert(String title, String content, SiteUser user) {
+
 		Notice notices = new Notice();
 
 		// 현재 날짜를 LocalDateTime 객체로 가져오기
-		LocalDateTime currentDate = LocalDateTime.now();
-		
-		// LocalDateTime 객체를 원하는 형식의 문자열로 변환하기
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // 원하는 형식으로 포맷 지정
-		String date = currentDate.format(formatter);
-		
-		// 현재 인증된 사용자 정보 가져오기
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String id = authentication.getName();
 
-
-		notices.setId(id);
-		notices.setTitle(notice.getTitle());
-		notices.setContent(notice.getContent());
-		notices.setDate(date);
+		notices.setId(user);
+		notices.setTitle(title);
+		notices.setContent(content);
+		notices.setDate(LocalDateTime.now());
 		notices.setViewcnt(0);
 		this.noticeRepository.save(notices);
 
-		noticeRepository.save(notices);
 	}
 
-	
-	// 컬럼 값 수정 update
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/* UPDATE */
+	// 2. update
 	public void update(Notice notice) {
 		Notice notice2 = noticeRepository.findByNoticeId(notice.getNoticeId()).get();
 		// 가져온 글에 수정한 내용을 세팅한다.
 		notice2.setTitle(notice.getTitle());
 		notice2.setContent(notice.getContent());
+		notice2.setPostModifiedDate(LocalDateTime.now());
 
         // DB에 저장
 		noticeRepository.save(notice2);
 	}
 
-	// 전체 게시물 조회
-	public List<Notice> getList() {
-//		PageRequest pageable = PageRequest.of(0, 10); // 페이지 번호 0부터 시작
-//		Page<BoardItem> page = boardRepository.findAll(pageable);
-//		return page.getContent();
-		return this.noticeRepository.findAll();
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/* READ */
+	// 3. list
+	public Page<Notice> getList(int page) {
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("noticeId"));
+		PageRequest pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 페이지 번호 0부터 시작
+		return this.noticeRepository.findAll(pageable);
 	}
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// id값으로 삭제
+	/* DELETE */
+	// 4. delete
 	public void deleteId(Long noticeId) {
-		noticeRepository.deleteByNoticeId(noticeId);
+		this.noticeRepository.deleteByNoticeId(noticeId);
 	}
 
-	// id값으로 한건 조회
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// 5. detail
 	public Notice oneSelectView(Long noticeId) {
-		return noticeRepository.findByNoticeId(noticeId).orElse(null);
+		return this.noticeRepository.findByNoticeId(noticeId).orElse(null);
 	}
 
-	// id값으로 한건 조회 시 조회수 카운트
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// 6. 조회수 카운트
 	public void visit(Long noticeId) {
 		Notice notice = noticeRepository.findByNoticeId(noticeId).orElse(null);
 		if (notice != null) {
 			notice.setViewcnt(notice.getViewcnt() + 1);
 			noticeRepository.save(notice);
+		}
+	}
+
+	// 7. 아이디 한건 조회
+	public Notice getNotice(Long noticeId) {
+		Optional<Notice> notice = this.noticeRepository.findByNoticeId(noticeId);
+		if (notice.isPresent()) {
+			return notice.get();
+		} else {
+			throw new DataNotFoundException("question not found");
 		}
 	}
 
