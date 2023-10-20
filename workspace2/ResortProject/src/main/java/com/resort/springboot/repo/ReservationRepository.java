b9d59594a2d1f8f364c66aa651a729940f3e0baa
noheul1030 <papooa5140@gmail.com>
2023-10-20 20:54:56 +0900

    Feat: 예약 Read 쿼리 추가
    1. 전체조회
    2. 한건 조회
    3. 아이디 조회
    4. 날짜 조회

--- workspace2/ResortProject/src/main/java/com/resort/springboot/repo/ReservationRepository.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/repo/ReservationRepository.java
@@ -1,11 +1,27 @@
 package com.resort.springboot.repo;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.resort.springboot.domain.Reservation;
+import com.resort.springboot.domain.SiteUser;
 
 @Repository
-public interface ReservationRepository extends JpaRepository<Reservation,Long>{
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+	// 1. 전체 조회
+	Page<Reservation> findAll(Pageable pageable);
+
+	// 2. 한건 조회
+	Optional<Reservation> findByReservationId(Long user);
+
+	// 3. 아이디 조회
+	Page<Reservation> findBySiteUser(SiteUser siteUser, Pageable pageable);
+
+	// 4. 날짜 조회
+	Page<Reservation> findByDate(String date, Pageable pageable);
 }
