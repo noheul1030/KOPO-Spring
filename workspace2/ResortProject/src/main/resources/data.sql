161a17d5520ada4bc93fdb5cc2932ca8124071e0
noheul1030 <papooa5140@gmail.com>
2023-11-03 01:53:59 +0900

    Update data.sql

--- workspace2/ResortProject/src/main/resources/data.sql
+++ workspace2/ResortProject/src/main/resources/data.sql
@@ -1,12 +1,12 @@
 INSERT INTO room (room_id, type)
-SELECT * FROM (SELECT 1 AS room_id, 'topSuiteRoom' AS type) AS temp
-WHERE NOT EXISTS (SELECT * FROM room WHERE room_id = 1);
+SELECT * FROM (SELECT '1' AS room_id, 'topSuiteRoom' AS type) AS temp
+WHERE NOT EXISTS (SELECT * FROM room WHERE room_id = '1');
 INSERT INTO room (room_id, type)
-SELECT * FROM (SELECT 2 AS room_id, 'familyRoom' AS type) AS temp
-WHERE NOT EXISTS (SELECT * FROM room WHERE room_id = 2);
+SELECT * FROM (SELECT '2' AS room_id, 'familyRoom' AS type) AS temp
+WHERE NOT EXISTS (SELECT * FROM room WHERE room_id = '2');
 INSERT INTO room (room_id, type)
-SELECT * FROM (SELECT 3 AS room_id, 'businessRoom' AS type) AS temp
-WHERE NOT EXISTS (SELECT * FROM room WHERE room_id = 3);
+SELECT * FROM (SELECT '3' AS room_id, 'businessRoom' AS type) AS temp
+WHERE NOT EXISTS (SELECT * FROM room WHERE room_id = '3');
 INSERT INTO room (room_id, type)
-SELECT * FROM (SELECT 4 AS room_id, 'standardRoom' AS type) AS temp
-WHERE NOT EXISTS (SELECT * FROM room WHERE room_id = 4);
+SELECT * FROM (SELECT '4' AS room_id, 'standardRoom' AS type) AS temp
+WHERE NOT EXISTS (SELECT * FROM room WHERE room_id = '4');
