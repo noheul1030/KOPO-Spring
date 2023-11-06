400531bb6d2b192275288e4a580ccb11b25343fa
noheul1030 <papooa5140@gmail.com>
2023-11-07 02:07:43 +0900

    Update Room.java

--- workspace2/ResortProject/src/main/java/com/resort/springboot/domain/Room.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/domain/Room.java
@@ -4,15 +4,25 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "room")
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Room {
-	
+
 	@Id
 	@Column(name = "room_id")
 	private Integer roomId;
-	
+
 	@Column
 	private String type;
 
