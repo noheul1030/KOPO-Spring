cc7b77d0ee8ed52320de3aa3e8f22450a65554d1
noheul1030 <papooa5140@gmail.com>
2023-11-13 19:05:27 +0900

    Feat: searchAllColumns 기능 구현중

--- workspace2/ResortProject/src/main/java/com/resort/springboot/repo/ReservationRepository.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/repo/ReservationRepository.java
@@ -2,8 +2,10 @@ package com.resort.springboot.repo;
 
 import java.util.Optional;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -31,7 +33,22 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 	void deleteByReservationId(Long id);
 
 	// Search
-//	@Query("SELECT r FROM Reservation r JOIN r.reservationUser ru JOIN r.room rm WHERE CONCAT(rm.type, ru.id, ru.name, ru.phoneNumber, ru.email) LIKE %:search%")
+	@Query("select cast(reservation.reservationId as string), CONCAT(reservation.year, '-', reservation.month, '-', reservation.day) AS formatted_date, room.type, user.id, user.name, user.phoneNumber ,user.email from Reservation reservation "
+			+ "left outer join SiteUser user on reservation.reservationUser=user "
+			+ "left outer join Room room on reservation.roomId=room " 
+			+ "where "
+			+ "   cast(reservation.reservationId as string) like %:search% "
+			+ "   or reservation.year like %:search% "
+			+ "   or reservation.month like %:search% "
+			+ "   or reservation.day like %:search% " 
+			+ "   or room.type like %:search% "
+			+ "   or user.id like %:search% " 
+			+ "   or user.name like %:search% "
+			+ "   or user.phoneNumber like %:search% " 
+			+ "   or user.email like %:search% ")
+	Page<Reservation> findAllByKeyword(@Param("search") String search, Pageable pageable);
 
-//	Page<Reservation> searchAllColumns(String search, Pageable pageable);
+//	@Query("SELECT r FROM Reservation r JOIN r.reservationUser ru JOIN r.roomId rm WHERE CONCAT(rm.type, ru.id, ru.name, ru.phoneNumber, ru.email) LIKE :search")
+//	Page<Reservation> searchAllColumns(@Param("search") String search, Pageable pageable);
+
 }
--- workspace2/ResortProject/src/main/java/com/resort/springboot/service/ReservationService.java
+++ workspace2/ResortProject/src/main/java/com/resort/springboot/service/ReservationService.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,12 @@ import com.resort.springboot.domain.SiteUser;
 import com.resort.springboot.exception.DataNotFoundException;
 import com.resort.springboot.repo.ReservationRepository;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -54,18 +61,41 @@ public class ReservationService {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	/* READ */
+	public static Specification<Reservation> search(String search) {
+		return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Reservation> reservation, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);  // 중복을 제거 
+                Join<Reservation, SiteUser> user = reservation.join("user", JoinType.LEFT);
+                Join<Reservation, Room> room = reservation.join("room", JoinType.LEFT);
+
+                return cb.or(
+                        cb.equal(reservation.get("year"), search),        // year
+                        cb.equal(reservation.get("month"), search),      // month
+                        cb.equal(reservation.get("day"), search),        // day
+                        cb.like(room.get("type"), "%" + search + "%"),    // room.type
+                        cb.like(user.get("id"), "%" + search + "%"),      // user.id 
+                        cb.like(user.get("name"), "%" + search + "%"),    // user.name 
+                        cb.like(user.get("phoneNumber"), "%" + search + "%"),  // user.phoneNumber 
+                        cb.like(user.get("email"), "%" + search + "%")    // user.email 
+                );
+            }
+        };
+    }
 
 	// 1. list
-	public Page<Reservation> getList(int page, String sort, String search) {
-		if (search != null && !search.isEmpty()) {
-//			return reservationRepository.searchAllColumns(search, PageRequest.of(page, 10));
-			return getSortedList(page, sort);
-		} else {
-			return getSortedList(page, sort);
-		}
-	}
+//	public Page<Reservation> getList(int page, String sort, String search) {
+//        if (search != null && !search.isEmpty()) {
+//        	Specification<Reservation> spec = search(search);
+//        	return this.reservationRepository.findAll(spec, PageRequest.of(page, 10, Sort.by(sorts)));
+////            return this.reservationRepository.searchAllColumns(search, PageRequest.of(page, 10));
+//        } else {
+//            return getSortedList(page, sort);
+//        }
+//    }
 
-	public Page<Reservation> getSortedList(int page, String sort) {
+	public Page<Reservation> getList(int page, String sort, String search) {
 		List<Sort.Order> sorts = new ArrayList<>();
 
 		if (sort.equals("")) {
@@ -85,6 +115,10 @@ public class ReservationService {
 			sorts.add(Sort.Order.desc("year"));
 			sorts.add(Sort.Order.desc("month"));
 			sorts.add(Sort.Order.desc("day"));
+		}
+
+		if (search != null && !search.isEmpty()) {
+			return this.reservationRepository.findAllByKeyword(search, PageRequest.of(page, 10, Sort.by(sorts)));
 		}
 
 		PageRequest pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 페이지 번호 0부터 시작
